7a7d4d5004513bd8a00570f1fce1493a576cc2e6
Vladyslav Svietlov <vladyslavsvietlov@MacBook-Pro-Vladyslav.local>
2023-07-04 23:40:03 +0300

    Chapter - Automatic Deployments

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,6 +3,22 @@ EC2_INSTANCE_TYPE=${2:-t2.micro}
 REGION=${3:-eu-west-1}
 CLI_PROFILE=${4:-default}
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+#Deploy static resources
+echo -e "\n\n ============ Deploying setup.yml ============"
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 #Deploy the CloudFormation template
 echo -e "\n\n ============ Deploying main.yml ============"
 aws cloudformation deploy \
@@ -17,6 +33,6 @@ aws cloudformation deploy \
 
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --profile default \
+    --profile ${CLI_PROFILE} \
     --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
